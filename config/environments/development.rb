6359061e8ab568bb0b9cf9f7a195f7f1480c6abf
aaronleechan <aaron171091@gmail.com>
2017-05-08 12:45:50 -0700

    change back to local

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,9 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = true
+  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -52,7 +52,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { :host =>'https://findbest.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
