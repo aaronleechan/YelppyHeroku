f2ece95c31e8f19120cc270c4c658c456c3f55d5
aaronleechan <aaron171091@gmail.com>
2017-05-08 18:18:48 -0700

    te

--- config/environment.rb
+++ config/environment.rb
@@ -4,16 +4,5 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-  config.action_mailer.default_url_options = { :host => 'https://findbest.herokuapp.com/' }
 
-  config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-      :address              => 'smtp.gmail.com',
-      :port                 => 587,
-      :user_name            => 'bestfind247@gmail.com',
-      :password             => 'greentea4',
-      :authentication       => 'plain',
-      :enable_starttls_auto => true
-  }
 
