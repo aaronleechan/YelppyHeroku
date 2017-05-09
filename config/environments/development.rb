fc68d074be484ede64bd7d01b95eddc5a22827a2
aaronleechan <aaron171091@gmail.com>
2017-05-08 17:17:00 -0700

    change back

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  #config.action_mailer.default_url_options = { :host => 'https://findbest.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'https://findbest.herokuapp.com/' }
 
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
