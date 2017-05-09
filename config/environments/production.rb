8e5b6e36e43dc30ad4db2277d61b3b2f3a8cb251
aaronleechan <aaron171091@gmail.com>
2017-05-08 18:12:36 -0700

    add rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,8 +34,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  config.action_mailer.smtp_settings = { port: 25 }
-
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
